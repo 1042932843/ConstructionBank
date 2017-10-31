02be797a2b8e84a9c07dd055f5e7272948116e5d
HDusky <1042932843@qq.com>
2017-10-31 17:50:43 +0800

    对欢迎界面的处理

---
+++ src/main/java/com/clpays/tianfugou/Module/Welcome/viewPage/Fragment4.java
@@ -0,0 +1,38 @@
+package com.clpays.tianfugou.Module.Welcome.viewPage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+
+import com.clpays.tianfugou.Module.Major.Home.HomePageActivity;
+import com.clpays.tianfugou.R;
+import com.clpays.tianfugou.Utils.Animation.AnimationUtil;
+
+
+public class Fragment4 extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		View view = inflater.inflate(R.layout.fragment_4, container, false);
+		view.findViewById(R.id.tvInNew).setOnClickListener(
+				new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						// TODO Auto-generated method stub
+						startActivity(new Intent(getActivity(),
+								HomePageActivity.class));
+						AnimationUtil.finishActivityAnimation(getActivity());
+					}
+				});
+		return view;
+	}
+
+}
---
+++ src/main/java/com/clpays/tianfugou/Module/Welcome/viewPage/ViewPagerActivity.java
@@ -0,0 +1,52 @@
+package com.clpays.tianfugou.Module.Welcome.viewPage;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.clpays.tianfugou.Adapter.ViewPagerAdapter;
+import com.clpays.tianfugou.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Name: ViewPagerActivity
+ * Author: Dusky
+ * QQ: 1042932843
+ * Comment: //欢迎界面
+ * Date: 2017-10-31 15:20
+ */
+public class ViewPagerActivity extends FragmentActivity {
+	private ViewPager mVPActivity;
+	private Fragment1 mFragment1;
+	private Fragment2 mFragment2;
+	private Fragment3 mFragment3;
+	private Fragment4 mFragment4;
+	private List<Fragment> mListFragment = new ArrayList<Fragment>();
+	private PagerAdapter mPgAdapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_viewpager);
+		initView();
+	}
+
+	private void initView() {
+		mVPActivity = (ViewPager) findViewById(R.id.vp_activity);
+		mFragment1 = new Fragment1();
+		mFragment2 = new Fragment2();
+		mFragment3 = new Fragment3();
+		mFragment4 = new Fragment4();
+		mListFragment.add(mFragment1);
+		mListFragment.add(mFragment2);
+		mListFragment.add(mFragment3);
+		mListFragment.add(mFragment4);
+		mPgAdapter = new ViewPagerAdapter(getSupportFragmentManager(),
+				mListFragment);
+		mVPActivity.setAdapter(mPgAdapter);
+	}
+}
